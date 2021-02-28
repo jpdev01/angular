b20b59ba64e76aa13e9ebf6124763badf6804f74
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-28 19:56:57 -0300

    Construçao do metodo responsavel por chamar o service direto do component => getLives

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Live } from 'src/app/shared/model/live.model';
+import { LiveService } from 'src/app/shared/service/live.service';
 
 @Component({
   selector: 'app-live-list',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  // listagem de lives que já aconteceram!
+  livesPrevious : Live[];
+
+  constructor(
+    public liveService: LiveService
+  ) { }
 
   ngOnInit(): void {
+    // inicialização do componente
+    this.getLives();
+  }
+
+  getLives() {
+    // Faz a requisição para a api externa
+    
+    //this.liveService.getLivesWithFlag('previous')
+    // terá como retorno um observable. Então iremos definir um subscribe que define o que irá acontecer quando esses dados chegarem dessa requisição da api externa.
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      // data.content pq o ResponsePageable tem o atributo content, que são as lives.
+      this.livesPrevious = data.content;
+
+    });
   }
 
 }
