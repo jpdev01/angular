a166f386d30d56016acb045d91a9f3e5183991d6
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-28 20:58:36 -0300

    Criacao e construcao do pipe para formatacao de data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
 import {HttpClientModule} from '@angular/common/http';
 import { LiveListComponent } from './views/home/live-list/live-list.component';
+import { LocalDateTimePipe } from './shared/pipe/local-date-time.pipe';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
     AppComponent,
     HomeComponent,
     LiveListComponent,
+    LocalDateTimePipe,
   ],
   imports: [
     BrowserModule,
