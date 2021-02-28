4c75e6d24fbef2def50eed3f5096adf4e1e7c477
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-28 16:07:25 -0300

    Arrumando declaraçao de compontent incorreta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { UserListComponent } from './user/list/user-list/user-list.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  directives: [ UserListComponent]
 })
 export class AppComponent {
   title = 'training-angular';
