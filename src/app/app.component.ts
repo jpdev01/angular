ce4aed5f30aa46125da0465512335583975c5304
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-28 15:53:31 -0300

    Criando o primeiro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { UserListComponent } from './user/list/user-list.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [ UserListComponent]
 })
 export class AppComponent {
   title = 'training-angular';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './user/list/list.component';
+import { UserListComponent } from './user/list/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
