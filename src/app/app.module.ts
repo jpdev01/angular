c5c4e3e2189170deceee24edd0135c793c0b0be4
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-28 16:29:49 -0300

    resetando para projeto template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './user/list/list.component';
-import { UserListComponent } from './user/list/user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
-    UserListComponent
   ],
   imports: [
     BrowserModule
