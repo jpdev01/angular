cd04fd35c832304ca857bd1f6b3bada487a1c541
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-03-02 23:15:45 -0300

    alteracao para a rota 'lives/' encaminhar para HomeComponent

--- src/app/views/lives/lives-routing.module.ts
+++ src/app/views/lives/lives-routing.module.ts
@@ -1,8 +1,9 @@
+import { HomeComponent } from './../home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LivesComponent } from './lives.component';
 
-const routes: Routes = [{ path: '', component: LivesComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
