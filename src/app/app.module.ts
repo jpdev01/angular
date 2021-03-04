c8c1f17bbc88a987826746dbac89d87007247964
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-03-03 22:45:04 -0300

    os imports precisam estar somente no seu modulo. retirado os imports desnecessarios de app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,23 +7,6 @@ import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
-import {HttpClientModule} from '@angular/common/http';
-// pipe formatador de data
-import { LocalDateTimePipe } from './shared/pipe/local-date-time.pipe';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatDialogModule} from '@angular/material/dialog';
-
-// módulos de formulário (angular/material)
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 @NgModule({
@@ -33,20 +17,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTabsModule,
-    MatCardModule,
-    MatChipsModule,
-    MatDialogModule,
-    HttpClientModule,
-    MatInputModule,
-    MatNativeDateModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatDatepickerModule,
-    MatProgressBarModule
+    HttpClient
   ],
   bootstrap: [AppComponent]
 })
