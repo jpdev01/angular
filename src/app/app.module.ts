96bb492d2a115cb9be828c204803d77c3df824ca
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-03-03 22:36:04 -0300

    app.module precisa das dependencias de toda a aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/lives/home/home.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
 import {HttpClientModule} from '@angular/common/http';
+import { LiveListComponent } from './views/lives/home/live-list/live-list.component';
 // pipe formatador de data
 import { LocalDateTimePipe } from './shared/pipe/local-date-time.pipe';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
 import { LiveFormDialogComponent } from './views/lives/home/live-form-dialog/live-form-dialog.component';
 
+// módulos de formulário (angular/material)
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    LiveListComponent,
     LocalDateTimePipe,
     LiveFormDialogComponent,
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatProgressBarModule
   ],
   providers: [LocalDateTimePipe],
   bootstrap: [AppComponent]
